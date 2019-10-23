2d6a9c9e7d2335ec41f35d9f5880808925f6a314
Toni Ristola <tristola@gmail.com>
2019-10-23 09:18:27 +0300

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Onko aamun juna myöhässä" />
+    <h1>Onko aamun R-juna myöhässä</h1>
+    <p>On.</p>
   </Layout>
 )
 
